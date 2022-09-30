60922a4767d97b4112afbb782144b9bf0d823073
Buse Yılmaz <65345966+Buseylmaz@users.noreply.github.com>
2022-09-30 13:57:20 +0300

    Add files via upload

---
+++ ChangeColor.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeColor : MonoBehaviour
+{
+
+    public GameObject sphere;
+    public int skor;
+  
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            skor += 10;
+
+            if (skor >= 100)
+            {
+                ColorChange();
+            }
+        }
+
+     //Klavyeden space tuşuna basarak skoru 10 10 artır skor 100 gelince ChangeColor metodunu çalıştır.
+    }
+
+
+    void ColorChange()
+    {
+        GameObject.Find("Sphere").GetComponent<MeshRenderer>().material.color = new Color(0, 0, 0);
+        //Kod açıklama:Shepere adlı gameobject bul.Sphere'nin meshrenderer ulaş ve material.color kısmına yeni renk ata.
+    }
+}
